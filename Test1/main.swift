7b137f4725ad65ed66845e2b233e25ba02736f26
gt7270a <prc-lh138-05@prc-lh138-05s-imac.local>
2016-07-11 13:15:56 -0500

    Initial Commit

---
+++ Test1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Test1
+//
+//  Created by PRC-LH138-05 on 7/11/16.
+//  Copyright © 2016 PRC-LH138-05. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
